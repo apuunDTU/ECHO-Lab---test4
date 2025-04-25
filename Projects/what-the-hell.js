c07b430b7b727e9c2e6ece1a22e20569d981d689
apuunDTU <apuun@dtu.dk>
2025-04-25 13:55:50 +0200

    Create what-the-hell.js

---
+++ Projects/what-the-hell.js
@@ -0,0 +1,37 @@
+//Project tile
+const noteData = {
+  title: "WHAT THE HELLLLLLLLL",
+  date: "DD.MM.YYYY", // Use this date format
+  image: "Assets/Test.png", // Put your image in the Assets folder
+  preview: "I don't know what is going on...",
+  researchTopics: [
+    "Topic 1",
+    "Topic 2",
+    "Topic 3"
+  ],
+  objectives: "What you want to achieve with this project",
+  methodology: "How you plan to do it or how you did it",
+  lessons: [
+    "What you learned 1",
+    "What you learned 2",
+    "What you learned 3"
+  ],
+  collaborators: [
+    "Person 1",
+    "Person 2",
+    "Person 3"
+  ],
+
+// Add your links
+projectLink: "https://www.linkedin.com/feed/", // Source link 1
+projectLinkText: "Framework Documentation", // Button text 1
+publicationLink: "https://www.linkedin.com/feed/", // Source link 2
+publicationLinkText: "Download Guidelines", // Button text 2
+
+// OPTIONAL: Change section titles (right column)
+  sectionTitles: {
+    topics: "Research Areas",
+    lessons: "Key Learnings",
+    collaborators: "Team Members"
+  }
+};
