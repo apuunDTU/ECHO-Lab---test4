1e3fd28909279c1cf111ce8b95727975b1b1de27
apuunDTU <apuun@dtu.dk>
2025-04-28 10:07:45 +0200

    Update what-the-hell.js

--- Projects/what-the-hell.js
+++ Projects/what-the-hell.js
@@ -3,11 +3,13 @@ const noteData = {
   title: "GROUNDING AI",
   date: "11.04.2025", // Use this date format
   image: "Assets/clusters-map.png", // Put your image in the Assets folder
-  preview: "I don't know what is going on...",
+  preview: "What are algorithms doing in science? We talk a lot about AI—often in terms of the spectacular, revolutionizing, and disruptive aspects of this technology. But beyond the controversies, AI is already embedded in the mundane, shaping decisions and processes in ways one might not notice or be aware of. From speech recognition to flight control or cybersecurity, from analyses of everything from tea quality and swim technique to hormone secretion and mammograms, AI and machine learning are woven into the fabric of science and society. Some of these applications raise concerns. Others barely enter public debate and remain in the scientific world. Should they? This map is an invitation to explore the landscape of AI in scientific literature and reflect on what it means for AI to be everywhere—and how we might engage with it."
+  ,
   researchTopics: [
-    "Topic 1",
-    "Topic 2",
-    "Topic 3"
+    "Public Engagement",
+    "Controversy mapping",
+    "LLMs for social research",
+    "Data visualisation"
   ],
   objectives: "What you want to achieve with this project",
   methodology: "How you plan to do it or how you did it",
